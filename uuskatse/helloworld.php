e1adac557d2143cdd908925e0fdbcca263266e19
tonyhaav <tonyhaav@greeny.cs.tlu.ee>
2016-02-11 12:43:30 +0200

    changed some lines

--- uuskatse/helloworld.php
+++ uuskatse/helloworld.php
@@ -1,1 +1,13 @@
-helloworld lalaa
+<?php
+
+	//variable starts with $
+	//string has to use quotation marks
+	// line ends with semicolon 
+	$first_name = "oh nana whats my name,";
+	$last_name = " its	tony";
+	
+	// show code to browser 
+	//for adding up numbers + sign
+	// for adding up strings 
+	echo $first_name.$last_name;
+?>
