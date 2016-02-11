db95af16a115b59e7ee321d2e05af2abf9763875
tonyhaav <tonyhaav@greeny.cs.tlu.ee>
2016-02-11 12:55:43 +0200

    added if variable

--- uuskatse/helloworld.php
+++ uuskatse/helloworld.php
@@ -10,4 +10,20 @@
 	//for adding up numbers + sign
 	// for adding up strings 
 	echo $first_name.$last_name;
+	
+	// > greater than
+	// < smaller than 
+	// >= greater than or equal
+	// <= smaller than or equal
+	// != not equal
+	
+	$age = 5;
+	//if age sis smaller than or equal to 6.
+	if($age <= 6) {
+		//it is true
+		echo "yes you can sleep at lunchtime";
+	}else{
+		//it is false
+		echo "no, you are at school";
+	}
 ?>
