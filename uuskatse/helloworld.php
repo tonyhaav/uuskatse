2b4f5b4383530908692b7c501a1bdeb005c8f337
tonyhaav <tonyhaav@greeny.cs.tlu.ee>
2016-02-11 13:27:56 +0200

    finishing and saving

--- uuskatse/helloworld.php
+++ uuskatse/helloworld.php
@@ -21,9 +21,28 @@
 	//if age sis smaller than or equal to 6.
 	if($age <= 6) {
 		//it is true
-		echo "yes you can sleep at lunchtime";
+		echo " yes you can sleep at lunchtime";
 	}else{
 		//it is false
 		echo "no, you are at school";
 	}
 ?>
+
+<br>
+
+<?php
+ //loop
+ 
+ for ($i=1; $i <= $age; $i=$i+1){
+	//if condition is true
+	echo $i;
+	
+	}
+ ?>
+ <br>
+ 
+ <?php
+	
+	echo "today is ".date("d.m.Y H:i ");
+	
+ ?>
